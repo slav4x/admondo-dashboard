4db2c4c142407296b5e301ae3f9f282ef177502b
Slava Khripushin <slava.cpa@yandex.ru>
2026-03-24 01:14:58 +0700

    feat: instructions

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.instructions-filter-select').forEach((label) => {
+    const select = label.querySelector('select');
+    const output = label.querySelector('p');
+
+    if (!select || !output) return;
+
+    const update = () => {
+      const selectedOption = select.options[select.selectedIndex];
+      output.textContent = selectedOption.text;
+    };
+
+    update();
+
+    select.addEventListener('change', update);
+  });
+});
